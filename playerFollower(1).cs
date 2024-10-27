5b42b4da2e7562ca46a955229bb54d5c937b7d5b
þorður skjaldberg <116281803+thordur-skjaldberg@users.noreply.github.com>
2024-10-27 21:53:36 +0000

    Add files via upload
    verkefni1.1

---
+++ playerFollower(1).cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerFollower : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 offset = new Vector3(0, 5, -10);
+
+    void Start()
+    {
+    }
+
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
